5b4c2cf98e292f6290acd4ddce29de03b1702bdf
arsetus <karkolub@gmail.com>
2025-10-31 13:41:55 +0000

    Update AddressesEU.py

--- AddressesEU.py
+++ AddressesEU.py
@@ -1,10 +1,10 @@
-my_x_address = 0xBA03D8 #static address
+my_x_address = 0xBA2418 #static address
 my_x_address_offset = []
-my_y_address = 0xBA03DC #static address
+my_y_address = 0xBA241C #static address
 my_y_address_offset = []
-my_z_address = 0xBA03E0 #static address
+my_z_address = 0xBA2420 #static address
 my_z_address_offset = []
-my_stats_address = 0xB9F990 #localplayer
+my_stats_address = 0xBA19D0 #localplayer
 my_hp_offset = 0x708 #offset from localplayer
 my_hp_max_offset = 0x710 #offset from localplayer
 my_mapX_dest_offset = 0x5B0 #offset from localplayer
@@ -12,13 +12,13 @@ my_mapY_dest_offset = 0x5B4 #offset from localplayer
 my_mp_offset = 0x738 #offset from localplayer
 my_mp_max_offset = 0x740 #offset from localplayer
 my_rest_offset = 0x760 #offset from localplayer
-my_name_offset = 0x248 #offset from localplayers
+my_name_offset = 0x248 #offset from localplayer
 my_skinID_offset = 0xBC #offset from localplayer
 my_skin_offset = 0x140 #offset from localplayer
 my_dir_offset = 0xB4 #offset from localplayer
 
 # Target Addresses
-attack_address = 0xB9F998 #static address
+attack_address = 0xBA19D8 #static address
 attack_address_offset = []
 target_name_offset = 0x90 #offset from target attack address
 target_x_offset = 0x50 #offset from target attack address
