30854c53de5b6f705dc81b844c7dcd8750e7d180
arsetus <karkolub@gmail.com>
2025-11-24 19:33:17 +0000

    Create Offsets.py

---
+++ Offsets.py
@@ -0,0 +1,22 @@
+my_x_address = 0x50 #static address
+my_y_address = 0x54 #static address
+my_z_address = 0x58 #static address
+my_hp_offset = 0x708 #offset from localplayer
+my_hp_max_offset = 0x710 #offset from localplayer
+my_mapX_dest_offset = 0x5B0 #offset from localplayer
+my_mapY_dest_offset = 0x5B4 #offset from localplayer
+my_mp_offset = 0x738 #offset from localplayer
+my_mp_max_offset = 0x740 #offset from localplayer
+my_rest_offset = 0x760 #offset from localplayer
+my_name_offset = 0x248 #offset from localplayers
+my_skinID_offset = 0xBC #offset from localplayer
+my_skin_offset = 0x140 #offset from localplayer
+my_dir_offset = 0xB4 #offset from localplayer
+
+# Target Addresses
+attack_address_offset = []
+target_name_offset = 0x90 #offset from target attack address
+target_x_offset = 0x50 #offset from target attack address
+target_y_offset = 0x54 #offset from target attack address
+target_z_offset = 0x58 #offset from target attack address
+target_hp_offset = 0xB0 #offset from target attack address
